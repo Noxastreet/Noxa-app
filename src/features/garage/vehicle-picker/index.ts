5dd00df15214087bdcebf309304557169275acbf
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-08-07 17:07:54 +0300

    feat(garage): export vehicle picker foundation

---
+++ src/features/garage/vehicle-picker/index.ts
@@ -0,0 +1,20 @@
+export { vehiclePickerMotionKey, isVehiclePickerMotionKey } from "./keys";
+export {
+  getVehicleGenerationPickerItems,
+  getVehicleMakePickerItems,
+  getVehicleModelPickerItems,
+  getVehicleYearPickerItems,
+  hasVehiclePickerGenerationStep,
+  isValidVehiclePickerSelection,
+} from "./selectors";
+export {
+  emptyVehiclePickerSelection,
+  type VehicleGenerationPickerItem,
+  type VehicleMakePickerItem,
+  type VehicleModelPickerItem,
+  type VehiclePickerCardItem,
+  type VehiclePickerCardKind,
+  type VehiclePickerSelection,
+  type VehiclePickerStep,
+  type VehicleYearPickerItem,
+} from "./types";
