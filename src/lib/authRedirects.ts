52346d63d429293799ce0fe41eb5d7c7cf492c21
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-09-14 10:07:05 +0300

    Add shared auth redirect constants

---
+++ src/lib/authRedirects.ts
@@ -0,0 +1,3 @@
+export const AUTH_CALLBACK_REDIRECT_URI = 'noxa://auth/callback';
+export const PASSWORD_RECOVERY_REDIRECT_URI = 'noxa://reset-password';
+export const AUTH_EMAIL_RESEND_COOLDOWN_SECONDS = 60;
